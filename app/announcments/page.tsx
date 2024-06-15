44545fdf8c575b2d8657599d2bd9b0032215a103
Andrew Levy <andrewlevy37@gmail.com>
2024-06-15 15:50:36 -0400

    added page for time-based announcments

---
+++ app/announcments/page.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+
+const timeToAnnouncmentMap = {
+  "9:00": "class",
+  "10:00": "game",
+  "10:30": "class",
+  "11:15": "lunch",
+  "12:15": "game",
+  "12:30": "class",
+  "1:30": "game",
+  "2:00": "elective",
+  "3:30": "outdoor",
+  "5:00": "dinner",
+};
+
+export default function Announcments() {
+  useEffect(() => {});
+}
