f749219055218292e4925090e02b648c3bb9223c
Sam Sherman <samsher4606@gmail.com>
2024-06-22 14:47:29 -0400

    fixed??

--- app/actions/google-sheets.action.ts
+++ app/actions/google-sheets.action.ts
@@ -8,12 +8,12 @@ export async function getSheetData() {
   const glAuth = await google.auth.getClient({
         projectId: "summerapp-427120",
         credentials: {
-            "type": configData.type,
-            "project_id": configData.project_id,
-            "private_key_id": configData.private_key_id,
+            "type": "service_account",
+            "project_id": "summerapp-427120",
+            "private_key_id": "1f644853d0a7ce0f0d056313999a93e2935c1561",
             "private_key": process.env.GOOGLE_API_KEY,
-            "client_email": configData.client_email,
-            "universe_domain": configData.universe_domain
+            "client_email": "devaccount@summerapp-427120.iam.gserviceaccount.com",
+            "universe_domain": "googleapis.com"
         },
         scopes: ["https://www.googleapis.com/auth/spreadsheets"],
     });
